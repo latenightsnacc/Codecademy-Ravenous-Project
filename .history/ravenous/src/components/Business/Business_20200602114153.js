5b8a9833344382aea4c4766c53484576b8f28674
Deborah <54311514+latenightsnacc@users.noreply.github.com>
2020-06-02 11:43:44 -0400

    Completed the project.

--- .history/ravenous/src/components/Business/Business_20200602114153.js
+++ .history/ravenous/src/components/Business/Business_20200602114153.js
@@ -12,14 +12,14 @@ class Business extends React.Component {
                 <h2>{this.props.business.name}</h2>
                 <div className="Business-information">
                     <div className="Business-address">
-                    <p>{this.props.business.address}</p>
-                    <p>{this.props.business.city}</p>
-                    <p>{ this.props.business.state } { this.props.business.zipCode }</p>
+                    <p>{this.props.address}</p>
+                    <p>{this.props.city}</p>
+                    <p>{ this.props.state } { this.props.zipCode }</p>
                     </div>
                     <div className="Business-reviews">
-                    <h3>{ this.props.business.category }</h3>
-                    <h3 className="rating">{ this.props.business.rating }</h3>
-                    <p>{ this.props.business.reviewCount }</p>
+                    <h3>{ this.props.category }</h3>
+                    <h3 className="rating">{ this.props.rating }</h3>
+                    <p>{ this.props.reviewCount }</p>
                     </div>
                 </div>
             </div>
--- .history/ravenous/src/components/BusinessList/BusinessList_20200531214034.js
+++ .history/ravenous/src/components/BusinessList/BusinessList_20200531214034.js
@@ -14,4 +14,5 @@ class BusinessList extends React.Component {
         );
     }
 }
+
 export default BusinessList;
