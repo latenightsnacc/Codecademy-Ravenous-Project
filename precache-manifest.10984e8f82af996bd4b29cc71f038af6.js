a15fa454c84f62b849d34889e6057b5e10156587
Deborah <54311514+latenightsnacc@users.noreply.github.com>
2020-06-02 12:04:09 -0400

    Updates

---
+++ precache-manifest.10984e8f82af996bd4b29cc71f038af6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc986d26812f1e7822e05cd6a70ab4e",
+    "url": "/Codecademy-Ravenous-Project/index.html"
+  },
+  {
+    "revision": "40dd5852eaedc77b1d29",
+    "url": "/Codecademy-Ravenous-Project/static/css/main.c4bdaf2f.chunk.css"
+  },
+  {
+    "revision": "10126e3e925687b0b41e",
+    "url": "/Codecademy-Ravenous-Project/static/js/2.b152fa3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Codecademy-Ravenous-Project/static/js/2.b152fa3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40dd5852eaedc77b1d29",
+    "url": "/Codecademy-Ravenous-Project/static/js/main.c6027bc1.chunk.js"
+  },
+  {
+    "revision": "90add969b7c7289bbe57",
+    "url": "/Codecademy-Ravenous-Project/static/js/runtime-main.5b98eb2d.js"
+  }
+]);
